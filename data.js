8cc9ec27a7ab7f6e2973f244e2c22f3ec5fde136
magvas <info@magvas>
2017-06-04 15:28:47 +0200

    changed DB user

--- data.js
+++ data.js
@@ -2,8 +2,8 @@ var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
   host     : '192.168.73.252',
-  user     : 'root',
-  password : '3X12=36',
+  user     : 'linfos',
+  password : 'linfos',
   database : 'LINFOS'
 });
 
