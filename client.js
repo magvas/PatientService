51a81a9ccbcbc9253baf145c3e5ddb44d1c3a6aa
magvas <info@magvas>
2017-06-15 01:05:48 +0200

    Added form to insert new patients

--- client.js
+++ client.js
@@ -1,11 +1,23 @@
+function Patient(first, last, gender) {
+    this.firstName = first;
+    this.lastName = last;
+    this.gender = gender;
+} 
+
+
+
 function getPatients(){
+    
+
     requestPatients(function(res){
 
         document.getElementById("output").innerHTML = "";
 
         for(var i = 0; i < res.length; i++){
-            var oldContent = document.getElementById("output").innerHTML;
-            document.getElementById("output").innerHTML = oldContent + res[i].PATID + " - " + res[i].LASTNAME + ", " + res[i].FIRSTNAME + " (" + res[i].GENDER + ")<br/>";
+            //var oldContent = document.getElementById("output").innerHTML;
+            //document.getElementById("output").innerHTML = oldContent + res[i].PATID + " - " + res[i].LASTNAME + ", " + res[i].FIRSTNAME + " (" + res[i].GENDER + ")<br/>";
+            var item = res[i].PATID + " - " + res[i].LASTNAME + ", " + res[i].FIRSTNAME + " (" + res[i].GENDER + ")";
+            addListItem("output",item);
         }    
     });
 }
@@ -54,4 +66,42 @@ function requestOrders(callback) {
 
     xhttp.open("GET", "http://localhost:3000/orders", true);
     xhttp.send();
+}
+
+function addPatient() {
+    var newPatient = new Patient(document.getElementById("txtLastname").value,document.getElementById("txtFirstname").value,document.getElementById("txtGender").value);    
+
+    var xhttp = new XMLHttpRequest();
+
+    var orders;
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {            
+            status(this.responseText);
+       }
+    };
+
+    var params = "?lastname=" + newPatient.lastName + "&firstname=" + newPatient.firstName + "&gender=" + newPatient.gender;
+    xhttp.open("GET", "http://localhost:3000/savepatient" + params, true,);
+    xhttp.send();
+}
+
+function showAddPatientDialog(){
+    document.getElementById("patientInput").style.visibility = "visible";
+}
+
+function status(statusText){
+    document.getElementById("txtStatus").innerHTML = statusText;
+}
+
+function addListItem(nameOfList,item) {     
+    var outputList = document.getElementById(nameOfList);    
+    var newItem = document.createElement("li");
+    newItem.innerText = item;
+    outputList.appendChild(newItem);
+
+    // var listItem = document.createElement("a");
+    //     listItem.setAttribute("href", "#");
+    //     listItem.innerText = listItems[i].Fullname;
+    //     _listScreen.appendChild(listItem);
 }
