817f4c9dc3348b8b6e5dbbadb102aa15575b0e22
magvas <info@magvas>
2017-11-19 00:54:19 +0100

    Delete useless file

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-var mongoClient      = require('mongodb').MongoClient;
-
-mongoClient.connect('mongodb://homeserver:27017/laboratoy',function (err,db) {
-   if (err) {
-       return console.dir(err)
-   } else {
-       var collection = db.collection('patients');
-   } 
-});
